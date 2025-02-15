0b44bc8ac3e5a6c90c75d9ad43231a072c9cd0a0
Fofanova Anastasiya <portable.tem@gmail.com>
2025-02-15 15:51:56 +0300

    change TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,36 +19,37 @@ public class TestBase {
 
     @BeforeAll
     static void configurationBrowser() {
-        Configuration.browser = System.getProperty("browser");
-        Configuration.browserVersion = System.getProperty("browserVersion");
-        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+       Configuration.browser = System.getProperty("browser");
+       Configuration.browserVersion = System.getProperty("browserVersion");
+       Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.baseUrl = "https://petrovich.ru";
         Configuration.pageLoadStrategy = "eager";
-       Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@" + System.getProperty("remoteHost") + "/wd/hub";
+      Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@" + System.getProperty("remoteHost") + "/wd/hub";
+//
+       DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("selenoid:options", Map .<String, Object>of(
+              "enableVNC", true,
+               "enableVideo", true
+      ));
+      Configuration.browserCapabilities = capabilities;
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-       capabilities.setCapability("selenoid:options", Map .<String, Object>of(
-               "enableVNC", true,
-                "enableVideo", true
-       ));
-       Configuration.browserCapabilities = capabilities;
-
-       SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+      SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
    @BeforeEach
    void addListener(){
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remoteReadTimeout = 5000;
     }
 
     @AfterEach
     public void afterEach() {
-        Attach.addScreenshot();
-        Attach.addVideo();
-        if (!System.getProperty("browser").equalsIgnoreCase("firefox")) {
-            Attach.pageSource();
-            Attach.getBrowserLogs();
-        }
+       Attach.addScreenshot();
+       Attach.addVideo();
+       if (!System.getProperty("browser").equalsIgnoreCase("firefox")) {
+           Attach.pageSource();
+           Attach.getBrowserLogs();
+       }
         Selenide.closeWebDriver();
 
     }
